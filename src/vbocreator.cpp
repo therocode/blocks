43ac443e9808bbea3f189ade1ddb081bed912d17
Tobias Widlund <widlundtobias@gmail.com>
2013-10-09 17:04:10 +0200

    reating several chunks, and chunks have a location

--- src/vbocreator.cpp
+++ src/vbocreator.cpp
@@ -6,6 +6,9 @@ ChunkVBO VBOCreator::generateChunkVBO(const Chunk& chunk) const
 {
 	ChunkVBO vbo;
 
+    const ChunkCoordinate location = chunk.getLocation();
+
+    glm::vec3 chunkOffset(location.x * chunkWidth, location.y * chunkWidth, location.z * chunkWidth);
 
 	const VoxelTypeArray& voxelTypes = chunk.getVoxelTypes();
 
@@ -13,10 +16,10 @@ ChunkVBO VBOCreator::generateChunkVBO(const Chunk& chunk) const
 		for(int y = 0; y < 10; y++){
 			//haha nothing
 			Rectangle r;
-			r.setPosition(0, i,     i + 1 + y , 2);
-			r.setPosition(1, i,     i + y     , 2);
-			r.setPosition(2, i + 1, i + y      ,2);
-			r.setPosition(3, i + 1, i + 1 + y  ,2);
+			r.setPosition(0, i + chunkOffset.x,     i + 1 + y + chunkOffset.y , 2 + chunkOffset.z);
+			r.setPosition(1, i + chunkOffset.x,     i + y      + chunkOffset.y, 2 + chunkOffset.z);
+			r.setPosition(2, i + 1 + chunkOffset.x, i + y       + chunkOffset.y,2 + chunkOffset.z);
+			r.setPosition(3, i + 1 + chunkOffset.x, i + 1 + y   + chunkOffset.y,2 + chunkOffset.z);
 			r.setColor((float)(rand()%255) /255.f, (float)(rand()%255) /255.f, (float)(rand()%255) /255.f);
 			r.calculateNormal();
 			vbo.PushRectangle(r);
