e8bfc85822dc93a3a33a781db157f0db0a80d16e
kim <kim@localhost.localdomain>
2013-10-09 17:44:03 +0800

    unbroken now, but still not working, just commiting to not be a douchebag

--- src/messages.h
+++ src/messages.h
@@ -5,4 +5,4 @@ struct ChunkCreated_tag{};//                               coordinate           
 using ChunkCreatedMessage = fea::Message<ChunkCreated_tag, const ChunkCoordinate*, const Chunk*>;
 
 struct WindowResize_tag{};//                               width                 height
-using WindowResizeMessage = fea::Message<WindowResize_tag, const uint32_t width, const uint32_t height>;
+using WindowResizeMessage = fea::Message<WindowResize_tag, const uint32_t, const uint32_t>;
