77a72e181da4817a038c99347950b16d0d9ca6a3
kim <kim@localhost.localdomain>
2013-10-11 00:36:24 +0800

    added inputactions.h to cmakelists and repo

--- src/input/inputadaptor.h
+++ src/input/inputadaptor.h
@@ -1,13 +1,16 @@
 #include <featherkit/userinterface.h>
+#include <featherkit/messaging.h>
 #include <SFML/Window.hpp>
 
 class InputAdaptor
 {
     public:
-        InputAdaptor(sf::Window& sfw);
+        InputAdaptor(sf::Window& sfw, fea::MessageBus& b);
+        void update();
 
     private:
         sf::Window& sfWindow;
         fea::InputHandler inputHandler;
         fea::ActionHandler<std::string> actionHandler;
+        fea::MessageBus bus;
 };
