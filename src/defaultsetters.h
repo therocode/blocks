f65753646b43d64d98ef31653209b0496955eb08
Tobias Widlund <widlundtobias@gmail.com>
2013-10-11 18:14:36 +0200

    added entity data files and default setters

---
+++ src/defaultsetters.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+#include <featherkit/entitysystem.h>
+#include <glm/glm.hpp>
+
+void vec3Setter(std::string attribute, std::vector<std::string>& arguments, fea::WeakEntityPtr entity);
