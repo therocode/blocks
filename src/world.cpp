d336e2b4fe72e07fceac4ce9019df791227c0b31
Tobias Widlund <widlundtobias@gmail.com>
2013-10-12 19:59:19 +0200

    elefant rain that has incorrect collisioons right now

--- src/world.cpp
+++ src/world.cpp
@@ -22,10 +22,15 @@ void World::initialise()
     entitySystem.addController(std::unique_ptr<EntityController>(new CollisionController(bus, worldInterface)));
     entitySystem.addController(std::unique_ptr<EntityController>(new GfxController(bus, worldInterface)));
 
-
-    fea::EntityPtr human = entitySystem.spawnEntity("human").lock();
-    human->setAttribute<glm::vec3>("position", glm::vec3(0.0f, 100.0f, 0.0f));
-    entitySystem.attachEntity(human);
+    for(int x = 0; x < 50; x++)
+    {
+        for(int y = 0; y < 50; y++)
+        {
+            fea::EntityPtr human = entitySystem.spawnEntity("human").lock();
+            human->setAttribute<glm::vec3>("position", glm::vec3(5.0f * x - 50, 100.0f, 5.0f * y - 50));
+            entitySystem.attachEntity(human);
+        }
+    }
 }
 
 void World::update()
