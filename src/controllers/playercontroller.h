1aac957e16187b5d419132de2c6ce9265ee63da6
kim <kim@localhost.localdomain>
2013-10-14 19:52:18 +0800

    actually did it

---
+++ src/controllers/playercontroller.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//message bus,
+//takes inputaction messages
+//deals with them
+//create camera instruction message --> go to the renderer?
+
+class PlayerController()
+{
+    public:
+        PlayerController();
+    private:
+
+}
