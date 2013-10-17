94cf8739f381cc8720f6717a5305186d54de9a8d
Tobias Widlund <widlundtobias@gmail.com>
2013-10-17 02:30:16 +0200

    added fix

--- src/script/scriptcallback.cpp
+++ src/script/scriptcallback.cpp
@@ -2,10 +2,10 @@
 
 void parameterFunction(asIScriptContext* context, std::size_t index, int value) 
 {
-    context->SetArgDWord(index, value); assert( r >= 0 );
+    int32_t r = context->SetArgDWord(index, value); assert( r >= 0 );
 }
  
 void parameterFunction(asIScriptContext* context, std::size_t index, float value) 
 {
-    context->SetArgFloat(index, value); assert( r >= 0 );
+    int32_t r = context->SetArgFloat(index, value); assert( r >= 0 );
 }
