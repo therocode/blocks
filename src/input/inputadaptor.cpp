17b824ae451a9daf41c65c0740956dc9baa64f41
Tobias Widlund <widlundtobias@gmail.com>
2013-10-25 21:04:26 +0200

    input adaptor now gets the right playerID

--- src/input/inputadaptor.cpp
+++ src/input/inputadaptor.cpp
@@ -12,7 +12,8 @@
 	InputAdaptor::InputAdaptor(sf::Window& sfw, fea::MessageBus& b)
 :   inputHandler(new fea::util::SDL2InputBackend),
 	sfWindow(sfw),
-	bus(b)
+    mPlayerId(-1),
+	mBus(b)
 {
 	fea::util::JsonActionIOHandler<std::string> jsonHandler;
 	jsonHandler.loadBindingsFile("data/bindings.json");
@@ -23,12 +24,17 @@
 	lastMouseY = 0;
 	first = true;
 	mouseDown = false;
+
+    mBus.addMessageSubscriber<PlayerIdMessage>(*this);   
+}
+
+InputAdaptor::~InputAdaptor()
+{
+    mBus.removeMessageSubscriber<PlayerIdMessage>(*this);   
 }
 
 void InputAdaptor::update()
 {
-	size_t playerId = 0; //this needs changing
-
 	fea::Event event;
 	std::string action;
 
@@ -39,7 +45,7 @@ void InputAdaptor::update()
 	{
 		if(event.type == fea::Event::CLOSED)
 		{
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::QUIT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::QUIT));
 		}
 		else if(event.type == fea::Event::MOUSEMOVED){
 			if(!first)
@@ -51,7 +57,7 @@ void InputAdaptor::update()
 					float sensitivity = 0.2f;
 					pitch *= sensitivity;
 					yaw   *= sensitivity;
-					bus.sendMessage<PlayerPitchYawMessage>(PlayerPitchYawMessage(playerId, pitch, yaw));
+					mBus.sendMessage<PlayerPitchYawMessage>(PlayerPitchYawMessage(mPlayerId, pitch, yaw));
 				}
 				lastMouseX = event.mouseMove.x;
 				lastMouseY = event.mouseMove.y;
@@ -67,50 +73,55 @@ void InputAdaptor::update()
 		else if(event.type == fea::Event::MOUSEBUTTONPRESSED){
 			mouseDown = true;
 			//if(event.mouseButton.button == fea::Mouse::Button::LEFT){
-			//    bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::MOUSELEFT));
+			//    mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::MOUSELEFT));
 			//}
 		}
 		else if(event.type == fea::Event::MOUSEBUTTONRELEASED){
 			mouseDown = false;
 			//if(event.mouseButton.button == fea::Mouse::Button::LEFT){
-			//    bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPMOUSELEFT));
+			//    mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPMOUSELEFT));
 			//}
 		}
 		else if(event.type == fea::Event::RESIZED){
-			bus.sendMessage<WindowResizeMessage>(WindowResizeMessage(event.size.width, event.size.height));
+			mBus.sendMessage<WindowResizeMessage>(WindowResizeMessage(event.size.width, event.size.height));
 		}
 	}
 
 	while(actionHandler.pollAction(action))
 	{
 		if(action == "quit")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::QUIT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::QUIT));
 		else if(action == "forwards")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::FORWARDS));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::FORWARDS));
 		else if(action == "backwards")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::BACKWARDS));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::BACKWARDS));
 		else if(action == "left")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::LEFT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::LEFT));
 		else if(action == "right")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::RIGHT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::RIGHT));
 		else if(action == "jump")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::JUMP));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::JUMP));
 		else if(action == "crouch")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::CROUCH));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::CROUCH));
 		else if(action == "rebuild_scripts")
-			bus.sendMessage<RebuildScriptsRequestedMessage>(RebuildScriptsRequestedMessage('0'));
+			mBus.sendMessage<RebuildScriptsRequestedMessage>(RebuildScriptsRequestedMessage('0'));
 
 		else if(action == "stopforwards")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPFORWARDS));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPFORWARDS));
 		else if(action == "stopbackwards")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPBACKWARDS));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPBACKWARDS));
 		else if(action == "stopleft")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPLEFT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPLEFT));
 		else if(action == "stopright")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPRIGHT));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPRIGHT));
 		else if(action == "stopjump")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPJUMP));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPJUMP));
 		else if(action == "stopcrouch")
-			bus.sendMessage<PlayerActionMessage>(PlayerActionMessage(playerId, InputAction::STOPCROUCH));
+			mBus.sendMessage<PlayerActionMessage>(PlayerActionMessage(mPlayerId, InputAction::STOPCROUCH));
 	}
+}
+
+void InputAdaptor::handleMessage(const PlayerIdMessage& received)
+{
+    std::tie(mPlayerId) = received.data;
 }
--- src/input/inputadaptor.h
+++ src/input/inputadaptor.h
@@ -1,19 +1,24 @@
 #pragma once
 #include <featherkit/userinterface.h>
 #include <featherkit/messaging.h>
+#include "../entity/entitymessages.h"
 #include <SFML/Window.hpp>
 
-class InputAdaptor
+class InputAdaptor : public fea::MessageReceiver<PlayerIdMessage>
 {
     public:
         InputAdaptor(sf::Window& sfw, fea::MessageBus& b);
+        ~InputAdaptor();
         void update();
+        void handleMessage(const PlayerIdMessage& received) override;
 
     private:
         sf::Window& sfWindow;
-        fea::MessageBus& bus;
+        fea::MessageBus& mBus;
         fea::InputHandler inputHandler;
         fea::ActionHandler<std::string> actionHandler;
+
+        size_t mPlayerId;
 
         int32_t lastMouseX;
         int32_t lastMouseY;
