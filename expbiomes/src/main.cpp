2d42ed2204c25344aba4d9965e88857a647db2e3
Tobias Widlund <widlundtobias@gmail.com>
2013-11-30 15:18:13 +0100

    cold regions

--- expbiomes/src/main.cpp
+++ expbiomes/src/main.cpp
@@ -6,7 +6,7 @@
 #include <noise.h>
 #include <iostream>
 
-const int seed = 10;
+const int seed = 12;
 
 struct Range
 {
@@ -298,11 +298,16 @@ int main()
     storage.addBiome(new Biome("grassmountain", 0.4f, 0.6f, 0.4f,   Range(0.3f, 1.0f), Range(0.3f, 1.0f),  Range(0.7f, 0.9f)));
     storage.addBiome(new Biome("snowymountain", 1.0f, 1.0f, 1.0f,   Range(0.0f, 0.3f), Range(0.4f, 1.0f),  Range(0.7f, 0.9f)));
 
-    storage.addBiome(new Biome("forest", 0.0f, 0.4f, 0.0f,          Range(0.0f, 1.0f), Range(0.60f, 1.0f),  Range(0.2f, 0.7f)));
-    storage.addBiome(new Biome("plains", 0.2f, 0.7f, 0.0f,          Range(0.0f, 1.0f), Range(0.50f, 0.7f),  Range(0.2f, 0.7f)));
-    storage.addBiome(new Biome("steppe", 0.3f, 0.6f, 0.0f,          Range(0.0f, 1.0f), Range(0.20f, 0.60f),  Range(0.2f, 0.7f)));
-    storage.addBiome(new Biome("xeric", 1.0f, 0.8f, 0.2f,           Range(0.0f, 1.0f), Range(0.05f, 0.25f),  Range(0.2f, 0.7f)));
-    storage.addBiome(new Biome("desert", 1.3f, 1.3f, 0.2f,          Range(0.0f, 1.0f), Range(0.0f, 0.15f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("forest", 0.0f, 0.4f, 0.0f,         Range(0.35f, 1.0f),Range(0.60f, 1.0f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("taiga", 0.0f, 0.6f, 0.3f,          Range(0.1f, 0.5f), Range(0.60f, 1.0f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("plains", 0.2f, 0.7f, 0.0f,          Range(0.25f, 1.0f),Range(0.50f, 0.7f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("steppe", 0.3f, 0.6f, 0.0f,          Range(0.25f, 1.0f), Range(0.20f, 0.60f), Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("snowfield", 1.3f, 1.3f, 1.3f,       Range(0.1f, 0.3f), Range(0.20f, 1.0f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("arcticfield", 1.42f, 1.42f, 1.42f,  Range(0.0f, 0.13f), Range(0.20f, 1.0f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("xeric", 1.0f, 0.8f, 0.2f,           Range(0.35f, 1.0f), Range(0.05f, 0.25f), Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("desert", 1.3f, 1.3f, 0.2f,          Range(0.35f, 1.0f), Range(0.0f, 0.15f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("coldxeric", 1.f, 0.7f, 0.4f,       Range(0.0f, 0.4f), Range(0.05f, 0.25f),  Range(0.2f, 0.7f)));
+    storage.addBiome(new Biome("colddesert", 1.2f, 0.9f, 0.5f,      Range(0.0f, 0.4f), Range(0.0f, 0.15f),  Range(0.2f, 0.7f)));
 
     storage.addBiome(new Biome("ocean", 0.0f, 0.0f, 1.0f,           Range(0.2f, 1.0f), Range(0.0f, 1.0f),  Range(0.0f, 0.2f)));
     storage.addBiome(new Biome("arctic ocean", 0.0f, 0.9f, 1.0f,    Range(0.0f, 0.2f), Range(0.0f, 1.0f),  Range(0.0f, 0.2f)));
