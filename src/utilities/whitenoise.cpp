d53319f2409112f09de3249d2fad017e8b499566
Tobias Widlund <widlundtobias@gmail.com>
2013-12-02 21:04:09 +0100

    reverted noise to the repetitive one

--- src/utilities/whitenoise.cpp
+++ src/utilities/whitenoise.cpp
@@ -4,16 +4,10 @@
 
 float WhiteNoise::get2d(const float x, const float y, const uint8_t* perm) const
 {
-    int i = std::floor(x);
-    int j = std::floor(y);
+    int32_t i = (int32_t)std::floor(x) % 255;
+    int32_t j = (int32_t)std::floor(y) % 255;
 
-    // Work out the hashed gradient indices of the three simplex corners
-    int16_t ii = i & 255;
-    int16_t jj = j & 255;
-    int gi0 = perm[ii + perm[jj]];
+    uint32_t res = perm[i + perm[j]];
 
-    //std::cout << "gi0 is now " << gi0 << "\n";
-
-    // The result is scaled to return values in the interval [-1,1].
-    return ((float)gi0 / 128.0f) - 1.0f;
+    return (float) res / 255.0f;
 }
--- src/utilities/whitenoise.h
+++ src/utilities/whitenoise.h
@@ -5,6 +5,4 @@ class WhiteNoise
 {
     public:
         float get2d(const float x, const float y, const uint8_t* perm) const;
-    private:
-        uint32_t joinBits(uint16_t a, uint16_t b) const;
 };
