945daf8b5f9cdbb7794b8a8efef43c7fe5835e56
Fred <fredrik.sandq@gmail.com>
2013-12-04 15:01:48 +0100

    fix white noise %

--- src/utilities/whitenoise.cpp
+++ src/utilities/whitenoise.cpp
@@ -4,8 +4,8 @@
 
 float WhiteNoise::get2d(const float x, const float y, const uint8_t* perm) const
 {
-    uint8_t i = (int32_t)std::floor(x) % 255;
-    uint8_t j = (int32_t)std::floor(y) % 255;
+    uint8_t i = (int32_t)std::floor(x) % 256;
+    uint8_t j = (int32_t)std::floor(y) % 256;
 
     uint8_t res = perm[i + perm[j]];
 
