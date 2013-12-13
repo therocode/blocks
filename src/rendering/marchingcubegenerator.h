0998eeb13600eba03dfb784b8166e496f654fb25
jefvel <akskor-1@student.ltu.se>
2013-12-13 18:03:09 +0100

    Started but nothing implemented

---
+++ src/rendering/marchingcubegenerator.h
@@ -0,0 +1,11 @@
+#include "blockstd.h"
+//rmembemr http://http.developer.nvidia.com/GPUGems3/gpugems3_ch01.html
+//http://www.cc.gatech.edu/~bader/COURSES/GATECH/CSE6140-Fall2007/papers/LC87.pdf
+//http://paulbourke.net/geometry/polygonise/volexample.zip
+
+class MarchingCubeGenerator
+{
+    public:
+    
+    private:  
+};
