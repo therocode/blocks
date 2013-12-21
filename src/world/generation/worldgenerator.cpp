6125807c120b7845b935dee877a1bb7dc584e167
Tobias Widlund <widlundtobias@gmail.com>
2013-12-21 16:39:18 +1400

    naaaeee.... vafan

--- src/world/generation/worldgenerator.cpp
+++ src/world/generation/worldgenerator.cpp
@@ -4,40 +4,49 @@
 
 Chunk WorldGenerator::generateChunk(const ChunkCoord& chunkCoordinate, const Region& region) const
 {
-    Chunk newChunk;
+    Chunk newChunk(chunkCoordinate);
 
     VoxelCoord chunkVoxelCoord = worldToVoxel(chunkToWorld(chunkCoordinate));
+    std::cout << chunkVoxelCoord.z << " is z \n";
     VoxelCoord voxelCoord;
     RegionVoxelCoord regionCoord;
 
-    float chunkY = chunkToWorld(chunkCoordinate).y;
+    float chunkY = chunkVoxelCoord.y;
 
     //std::cout << "chunk is " << glm::to_string((glm::ivec3)chunkCoordinate) << "\n";
 
-    voxelCoord.x = chunkVoxelCoord.x;
-    for(int32_t x = 0; x < chunkWidth; x++)
+    for(int32_t y = 0; y < chunkWidth; y++)
     {
-        voxelCoord.y = chunkVoxelCoord.y;
-        for(int32_t y = 0; y < chunkWidth; y++)
+        voxelCoord.x = chunkVoxelCoord.x;
+        for(int32_t x = 0; x < chunkWidth; x++)
         {
             voxelCoord.z = chunkVoxelCoord.z;
             for(int32_t z = 0; z < chunkWidth; z++)
             {
                 //std::cout << "voxel coord for chunk is " << voxelCoord.x << " " << voxelCoord.y << " " << voxelCoord.z << "\n";
                 regionCoord = voxelToRegionVoxel(voxelCoord);
-    //if(chunkCoordinate == ChunkCoord(1,0,3))
+                //if(chunkCoordinate == ChunkCoord(1,0,3))
                 //std::cout << "region coord: " << regionCoord.x << " " << regionCoord.y << "\n";
-                float worldY = chunkY + y + x + z*2;//(float)(chunkY + y) + region.getHeightmap().getUnit(regionCoord.x, regionCoord.y) * 100.f;
+                float worldY = (float)(chunkY + y) + region.getHeightmap().getUnit(regionCoord.x, regionCoord.y) * 100.f;
                 if(worldY < 0.0f)
-                    newChunk.setVoxelType(x, y, z, 1);
+                {
+                    RegionCoord region = voxelToRegion(voxelCoord);
+                    if(region == RegionCoord(0,0))
+                        newChunk.setVoxelType(x, y, z, 2);
+                    else if(region == RegionCoord(-1, 0))
+                        newChunk.setVoxelType(x, y, z, 1);
+                    else if(region == RegionCoord(0, -1))
+                        newChunk.setVoxelType(x, y, z, 4);
+                    else if(region == RegionCoord(-1, -1))
+                        newChunk.setVoxelType(x, y, z, 1);
+                }
                 else
                     newChunk.setVoxelType(x, y, z, 0);
-                
+
                 voxelCoord.z++;
             }
-            voxelCoord.y++;
+            voxelCoord.x++;
         }
-        voxelCoord.x++;
     }
 
     return newChunk;
