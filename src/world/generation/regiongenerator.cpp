5b01772a29c6378f2c3da94a06250b89c78a2efa
root <root@ornithopod.pallkars.net>
2013-12-21 20:20:01 +0800

    almost

--- src/world/generation/regiongenerator.cpp
+++ src/world/generation/regiongenerator.cpp
@@ -1,13 +1,28 @@
 #include "regiongenerator.h"
+#include "utilities/noise.h"
 
 IntensityMap RegionGenerator::generateHeightmap(const RegionCoord& regionCoordinate) const
 {
     IntensityMap heightmap;
+    Noise simplex(899);
+    Noise simplex2(899 + 17);
+
+    glm::vec3 regionPos = regionToWorld(regionCoordinate);
 
     for(int x = 0; x < regionWidth * chunkWidth; x++)
     for(int y = 0; y < regionWidth * chunkWidth; y++)
     {
-        heightmap.setUnit(x, y, (float)(rand() % 1000) / 1000.f);
+        //float value = raw_noise_3d((float) x / 200.0f, (float) y / 200.0f, 10.5);
+        float value = simplex.simplexOctave2D((float) (x + regionPos.x) / 200.0f, (float) (y + regionPos.y) / 200.0f, 0.6f, 6);
+        value +=      simplex2.simplexOctave2D((float) (x + regionPos.x) / 800.0f, (float) (y + regionPos.y) / 800.0f, 0.6f, 6); //big height difference
+        //value = simplex.white2D((float) x / 2.0f, (float) y / 2.0f);
+
+        //float value = (perlin.GetValue((float) x / 200.0f, (float) y / 200.0f, 1000.5));
+        //std::cout << "value: " << value << "\n";
+        value = value * 1.8f;
+        value = (value + 1.0f) / 2.0f;
+        value = std::max(0.0f, std::min(value, 1.0f));
+        heightmap.setUnit(x, y, value);
     }
 
     return heightmap;
--- src/world/generation/worldgenerator.cpp
+++ src/world/generation/worldgenerator.cpp
@@ -10,7 +10,9 @@ Chunk WorldGenerator::generateChunk(const ChunkCoord& chunkCoordinate, const Reg
     VoxelCoord voxelCoord;
     RegionVoxelCoord regionCoord;
 
-    float chunkY = chunkCoordinate.y * chunkWidth;
+    float chunkY = chunkToWorld(chunkCoordinate).y;
+
+    std::cout << "chunk z is " << chunkCoordinate.z << "\n";
 
     voxelCoord.x = chunkVoxelCoord.x;
     for(int32_t x = 0; x < chunkWidth; x++)
@@ -21,7 +23,10 @@ Chunk WorldGenerator::generateChunk(const ChunkCoord& chunkCoordinate, const Reg
             voxelCoord.z = chunkVoxelCoord.z;
             for(int32_t z = 0; z < chunkWidth; z++)
             {
+    std::cout << "voxel z is " << voxelCoord.z << "\n";
+                //std::cout << "voxel coord for chunk is " << voxelCoord.x << " " << voxelCoord.y << " " << voxelCoord.z << "\n";
                 regionCoord = voxelToRegionVoxel(voxelCoord);
+                //std::cout << "region coord: " << regionCoord.x << " " << regionCoord.y << "\n";
                 float worldY = (float)(chunkY + y) + region.getHeightmap().getUnit(regionCoord.x, regionCoord.y) * 100.0f;
                 if(worldY < 0.0f)
                     newChunk.setVoxelType(x, y, z, 1);
