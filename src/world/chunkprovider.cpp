7fb5a757f6ab18a1c41c9db1e260655f70b633c8
Tobias Widlund <widlundtobias@gmail.com>
2013-12-28 08:47:58 +0800

    hoj

--- src/world/chunkprovider.cpp
+++ src/world/chunkprovider.cpp
@@ -30,7 +30,7 @@ void ChunkProvider::handleMessage(const ChunkRequestedMessage& received)
     Chunk newChunk = mWorldGenerator.generateChunk(chunkCoordinate, region);
 
     //mBus.sendMessage(LogMessage("loading modifications for chunk" + glm::to_string((glm::ivec3)chunkCoordinate), "file", LogLevel::VERB));
-    //mModManager.loadMods(newChunk);
+    mModManager.loadMods(newChunk);
     uint64_t timestamp = 0; //get proper timestamp later
 
     mBus.sendMessage(ChunkDeliverMessage(chunkCoordinate, newChunk)); //sends the finished chunk to be kept by whatever system
