5d185cc65bfe6a5ee313ff9238eec165b64fb032
Tobias Widlund <widlundtobias@gmail.com>
2014-06-24 15:05:33 +0800

    2D instead of 3D

--- expbiomes/src/generator.hpp
+++ expbiomes/src/generator.hpp
@@ -4,7 +4,15 @@
 class Generator
 {
     public:
+        Generator();
         void generateCoordinate(float x, float y, float& height, float& rain, float& temperature, float& biome);
     private:
-        fea::Noise noise;
+        fea::Noise noise0;
+        fea::Noise noise1;
+        fea::Noise noise2;
+        fea::Noise noise3;
+        fea::Noise noise4;
+        fea::Noise noise5;
+        fea::Noise noise6;
+        fea::Noise noise7;
 };
