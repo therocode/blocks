f0533b5799f939a485a78d503f57b187d6ef4635
Tobias Widlund <widlundtobias@gmail.com>
2014-09-26 13:33:42 +0200

    removed unnecessary files

--- src/entity/defaultsetters.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <vector>
-#include <fea/entitysystem.hpp>
-#include <glm/glm.hpp>
-
-void vec3Setter(const std::string& attribute, const std::vector<std::string>& arguments, fea::WeakEntityPtr entity);
