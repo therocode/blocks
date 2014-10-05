651bc7b305b8848c8da19e847c90356568dbcde9
Tobias Widlund <widlundtobias@gmail.com>
2014-10-05 19:19:30 +0200

    added preliminary message

--- src/networking/networkingprotocol.hpp
+++ src/networking/networkingprotocol.hpp
@@ -1,6 +1,13 @@
 #include "../serializable.hpp"
 
+enum MessageType { CLIENT_JOIN_REQUESTED };
+
 //network protocol:
 struct ClientJoinRequestedMessage : public Serializable
 {
+    public:
+        std::vector<uint8_t> toBytes() const override;
+        void fromBytes(const std::vector<uint8_t>& bytes) override;
+    private:
+        ClientJoinRequestedMessage 
 };
