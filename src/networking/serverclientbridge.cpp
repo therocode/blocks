f6b9bdae137edcecf6a26d6fd434ae7d1d12f0ee
Tobias Widlund <widlundtobias@gmail.com>
2014-10-08 19:45:35 +0200

    readded accidentally deleted files

---
+++ src/networking/serverclientbridge.cpp
@@ -0,0 +1,36 @@
+#include "serverclientbridge.hpp"
+#include "package.hpp"
+
+bool ServerClientBridge::pollPackage(std::shared_ptr<BasePackage>& package)
+{
+    if(mIncoming.size())
+    {
+        package = mIncoming.front();
+        mIncoming.pop_front();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void ServerClientBridge::enqueuePackage(std::weak_ptr<BasePackage> package)
+{
+    std::lock_guard<std::mutex> lock(mOutGoingMutex);
+    mOutgoing.push_back(package.lock());
+}
+
+ServerClientBridge::~ServerClientBridge()
+{
+}
+
+void ServerClientBridge::disconnect()
+{
+    disconnected = true;
+}
+
+bool ServerClientBridge::isDisconnected()
+{
+    return disconnected;
+}
