e34ba6a40a241c87f75f5e2c24103cb02198787d
Lucas Lindström <ztargaser@gmail.com>
2014-12-21 17:29:01 +0100

    Removed interface methods from the extension interface.

--- src/script/interfaces/extensioninterface.cpp
+++ src/script/interfaces/extensioninterface.cpp
@@ -10,10 +10,4 @@ ExtensionInterface::ExtensionInterface(fea::MessageBus& bus, GameInterface& worl
 void ExtensionInterface::registerInterface(asIScriptEngine* engine)
 {
     int32_t r = engine->RegisterInterface("IExtension"); assert(r >= 0);
-
-    r = engine->RegisterInterfaceMethod("IExtension", "void chunkInitiallyGenerated(const uint32, const ChunkCoord &in, Chunk &in)"); assert(r >= 0);
-
-    r = engine->RegisterInterfaceMethod("IExtension", "void chunkCandidate(const uint32, const ChunkCoord &in, Chunk &in, const uint64)"); assert(r >= 0);
-
-    r = engine->RegisterInterfaceMethod("IExtension", "void chunkFinished(const uint32, const ChunkCoord &in, const Chunk &in)"); assert(r >= 0);
 }
