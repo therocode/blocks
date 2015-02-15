5204e8edcecd4f9e0f4968dc9f645ddd6d70aa79
Tobias Widlund <widlundtobias@gmail.com>
2015-02-16 00:57:42 +0100

    Wow, different animation offsets is almost finished

--- src/rendering/modelrenderer.cpp
+++ src/rendering/modelrenderer.cpp
@@ -74,7 +74,7 @@ void ModelRenderer::render(const Camera& camera, const glm::mat4& perspective, c
 
         modelBufferStorage->vertexArray.bind();
 
-        uploadBatchData(modelIterator.second, camera, shader, model, *modelBufferStorage, {mCurFrame, mCurFrame + 10.0f, mCurFrame + 20.0f, mCurFrame + 30.0f});
+        uploadBatchData(modelIterator.second, camera, shader, model, *modelBufferStorage, {mCurFrame, mCurFrame + 30.0f, mCurFrame + 60.0f});
 
         for(const auto& mesh : modelBufferStorage->meshes)
         {
@@ -257,76 +257,84 @@ void ModelRenderer::uploadBatchData(const std::vector<ModelOrder>& modelOrders, 
             }
         }
 
-        int32_t numJoints = model.getJointStructure().size();
+        const auto& jointStructure = model.getJointStructure();
+        int32_t numJoints = jointStructure.size();
 
         //animation
-        std::vector<float> rotData(12 * maxBoneAmount * maxInstanceAmount);
-        std::vector<float> transData(4 * maxBoneAmount * maxInstanceAmount);
+        std::vector<float> rotData(12 * maxBoneAmount);
+        std::vector<float> transData(4 * maxBoneAmount);
         const Animation* animation = model.getAnimation();
-        std::vector<float> animationData;
+        std::vector<float> totalRotData;
+        std::vector<float> totalTransData;
         if(animation != nullptr)
         {
-            float localCurrentFrame = (mCurFrame / 60.0f) * animation->framerate;
-            int32_t numFrames = animation->frameAmount;
-            int32_t frame1 = (int32_t)std::floor(localCurrentFrame);// * (animation->framerate/60.0f));
-            int32_t frame2 = frame1 + 1;
-            float frameOffset = localCurrentFrame- frame1;
-            frame1 %= numFrames;
-            frame2 %= numFrames;
-
-            const auto rotation1 = animation->rotations.begin() + frame1 * numJoints;
-            const auto rotation2 = animation->rotations.begin() + frame2 * numJoints;
-            const auto translation1 = animation->translations.begin() + frame1 * numJoints;
-            const auto translation2 = animation->translations.begin() + frame2 * numJoints;
-
-
-            const auto& jointStructure = model.getJointStructure();
-
-            std::vector<glm::mat4x4> outputTransformation(numJoints);
-
-            for(int32_t i = 0; i < numJoints; i++)
+            std::cout << "will now loop " << instanceFrameData.size() << "times\n";
+            for(int32_t instanceFrameCounter = 0; instanceFrameCounter < instanceFrameData.size(); instanceFrameCounter++)
             {
-                int32_t rotIndex = i * 12;
-                int32_t transIndex = i * 4;
-                glm::mat3x3 rotation    = rotation1[i]    * (1.0f - frameOffset) + rotation2[i]    * frameOffset;
-                glm::vec3 translation = translation1[i] * (1.0f - frameOffset) + translation2[i] * frameOffset;
+                float localCurrentFrame = (instanceFrameData[instanceFrameCounter] / 60.0f) * animation->framerate;
+                int32_t numFrames = animation->frameAmount;
+                int32_t frame1 = (int32_t)std::floor(localCurrentFrame);// * (animation->framerate/60.0f));
+                int32_t frame2 = frame1 + 1;
+                float frameOffset = localCurrentFrame- frame1;
+                frame1 %= numFrames;
+                frame2 %= numFrames;
 
-                glm::mat4x4 transformation = glm::translate(glm::mat4x4(rotation), translation);
-                transformation[3][0] = translation.x;
-                transformation[3][1] = translation.y;
-                transformation[3][2] = translation.z;
+                const auto rotation1 = animation->rotations.begin() + frame1 * numJoints;
+                const auto rotation2 = animation->rotations.begin() + frame2 * numJoints;
+                const auto translation1 = animation->translations.begin() + frame1 * numJoints;
+                const auto translation2 = animation->translations.begin() + frame2 * numJoints;
 
-                FEA_ASSERT(i > jointStructure[i], "Parent structure messed up. Joint " + std::to_string(i) + " has the parent " + std::to_string(jointStructure[i]) + ".");
-                if(jointStructure[i] >= 0)
+                std::vector<glm::mat4x4> outputTransformation(numJoints);
+
+                for(int32_t i = 0; i < numJoints; i++)
                 {
-                    outputTransformation[i] = outputTransformation[jointStructure[i]] * transformation;
-                }
-                else
-                {
-                    outputTransformation[i] = transformation;
-                }
+                    int32_t rotIndex = i * 12;
+                    std::cout << rotIndex << " is rotIndex because i is " << i << "\n";
+                    int32_t transIndex = i * 4;
+                    glm::mat3x3 rotation    = rotation1[i]    * (1.0f - frameOffset) + rotation2[i]    * frameOffset;
+                    glm::vec3 translation = translation1[i] * (1.0f - frameOffset) + translation2[i] * frameOffset;
 
-                const float* floatIter = glm::value_ptr(outputTransformation[i]);
-                rotData[rotIndex + 0]  = *floatIter;
-                rotData[rotIndex + 1]  = *(floatIter + 1);
-                rotData[rotIndex + 2]  = *(floatIter + 2);
-                //rotData[rotIndex + 3]  //skipped due to padding
-                rotData[rotIndex + 4]  = *(floatIter + 4);
-                rotData[rotIndex + 5]  = *(floatIter + 5);
-                rotData[rotIndex + 6]  = *(floatIter + 6);
-                //rotData[rotIndex + 7]  //skipped due to padding
-                rotData[rotIndex + 8]  = *(floatIter + 8);
-                rotData[rotIndex + 9]  = *(floatIter + 9);
-                rotData[rotIndex + 10] = *(floatIter + 10);
-                //rotData[rotIndex + 11]  //skipped due to padding
-                transData[transIndex + 0] = outputTransformation[i][3][0];
-                transData[transIndex + 1] = outputTransformation[i][3][1];
-                transData[transIndex + 2] = outputTransformation[i][3][2];
+                    glm::mat4x4 transformation = glm::translate(glm::mat4x4(rotation), translation);
+                    transformation[3][0] = translation.x;
+                    transformation[3][1] = translation.y;
+                    transformation[3][2] = translation.z;
+
+                    FEA_ASSERT(i > jointStructure[i], "Parent structure messed up. Joint " + std::to_string(i) + " has the parent " + std::to_string(jointStructure[i]) + ".");
+                    if(jointStructure[i] >= 0)
+                    {
+                        outputTransformation[i] = outputTransformation[jointStructure[i]] * transformation;
+                    }
+                    else
+                    {
+                        outputTransformation[i] = transformation;
+                    }
+
+                    const float* floatIter = glm::value_ptr(outputTransformation[i]);
+                    rotData[rotIndex + 0]  = *floatIter;
+                    rotData[rotIndex + 1]  = *(floatIter + 1);
+                    rotData[rotIndex + 2]  = *(floatIter + 2);
+                    //rotData[rotIndex + 3]  //skipped due to padding
+                    rotData[rotIndex + 4]  = *(floatIter + 4);
+                    rotData[rotIndex + 5]  = *(floatIter + 5);
+                    rotData[rotIndex + 6]  = *(floatIter + 6);
+                    //rotData[rotIndex + 7]  //skipped due to padding
+                    rotData[rotIndex + 8]  = *(floatIter + 8);
+                    rotData[rotIndex + 9]  = *(floatIter + 9);
+                    rotData[rotIndex + 10] = *(floatIter + 10);
+                    //rotData[rotIndex + 11]  //skipped due to padding
+                    transData[transIndex + 0] = outputTransformation[i][3][0];
+                    transData[transIndex + 1] = outputTransformation[i][3][1];
+                    transData[transIndex + 2] = outputTransformation[i][3][2];
+                }
+                totalRotData.insert(totalRotData.end(), rotData.begin(), rotData.end());
+                totalTransData.insert(totalTransData.end(), transData.begin(), transData.end());
             }
-            animationData.insert(animationData.end(), rotData.begin(), rotData.end());
-            animationData.insert(animationData.end(), transData.begin(), transData.end());
         }
         //animation end
+
+        std::vector<float> totalAnimData(totalRotData.size() + totalTransData.size());
+        auto nextEntry = std::copy(totalRotData.begin(), totalRotData.end(), totalAnimData.begin());
+        std::copy(totalTransData.begin(), totalTransData.end(), nextEntry);
         
         modelBufferStorage.colors.setData(colors);
         modelBufferStorage.textureIndices.setData(textureIndices);
@@ -338,7 +346,7 @@ void ModelRenderer::uploadBatchData(const std::vector<ModelOrder>& modelOrders, 
         modelBufferStorage.normalMatrix2.setData(normalMatrix2);
         modelBufferStorage.normalMatrix3.setData(normalMatrix3);
 
-        modelBufferStorage.animData.setData(animationData);
+        modelBufferStorage.animData.setData(totalAnimData);
 
         modelBufferStorage.vertexArray.bind();
 
@@ -350,6 +358,5 @@ void ModelRenderer::uploadBatchData(const std::vector<ModelOrder>& modelOrders, 
 
         GLint blockSize;
         glGetActiveUniformBlockiv(shader.getId(), blockIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
-
         std::cout << "size of block is " << blockSize << "\n";
 }
